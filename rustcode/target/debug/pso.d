3ebccf66f7523d43caf5fb3f4d8e62a14081bb6f
KK <Konrad_kraemer@yahoo.de>
2021-03-08 17:04:37 +0100

    seperated rust from fortran

---
+++ rustcode/target/debug/pso.d
@@ -0,0 +1,1 @@
+/home/konrad/Dokumente/1Privat/Programming/rust/pso/target/debug/pso: /home/konrad/Dokumente/1Privat/Programming/rust/pso/src/main.rs /home/konrad/Dokumente/1Privat/Programming/rust/pso/src/matrix.rs
