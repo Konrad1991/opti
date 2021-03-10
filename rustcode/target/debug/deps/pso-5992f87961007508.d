7c74cd25467efd245d3ac40df429ca9481070cf3
Konrad1991 <konrad_kraemer@yahoo.de>
2021-03-10 16:50:01 +0100

    removed unnecessary files. Added licence file

--- rustcode/target/debug/deps/pso-5992f87961007508.d
+++
@@ -1,6 +0,0 @@
-/home/konrad/Dokumente/Programming/rust/pso/target/debug/deps/pso-5992f87961007508: src/main.rs src/matrix.rs
-
-/home/konrad/Dokumente/Programming/rust/pso/target/debug/deps/pso-5992f87961007508.d: src/main.rs src/matrix.rs
-
-src/main.rs:
-src/matrix.rs:
--- rustcode/target/debug/pso.d
+++
@@ -1,1 +0,0 @@
-/home/konrad/Dokumente/1Privat/Programming/rust/pso/target/debug/pso: /home/konrad/Dokumente/1Privat/Programming/rust/pso/src/main.rs /home/konrad/Dokumente/1Privat/Programming/rust/pso/src/matrix.rs
